85de309450686fb53e2f07ba5550b6d3cbb15881
Jacob Koziej <jacobkoziej@gmail.com>
2023-10-04 15:15:00 -0400

    Determine `DIRNAME`

--- src/cogx/__init__.py
+++ src/cogx/__init__.py
@@ -3,6 +3,7 @@
 # __init__.py -- cog extensions
 # Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
 
+import os
 import typing
 
 from . import cog
@@ -14,4 +15,8 @@ __author__: typing.Final[str] = 'Jacob Koziej'
 __version__: typing.Final[str] = '0.0.0'
 
 
+DIRNAME: typing.Final[str] = os.path.dirname(cog.IN_FILE)
+
+
+del os
 del typing
