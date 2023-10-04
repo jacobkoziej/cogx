f9046d95bf95a63744ba65791e688e7bb2aa61b4
Jacob Koziej <jacobkoziej@gmail.com>
2023-10-04 00:31:30 -0400

    Add `__init__.py`

---
+++ src/cogx/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# __init__.py -- cog extensions
+# Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+
+import typing
+
+
+__author__: typing.Final[str] = 'Jacob Koziej'
+__version__: typing.Final[str] = '0.0.0'
+
+
+del typing
